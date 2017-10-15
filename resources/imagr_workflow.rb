bbb2985f2e6fea54bcb3129dba7e17b2f39255e0
Eric Hanko <v-erhank@microsoft.com>
2017-10-15 16:13:02 -0700

    Begin :initialize implementation

--- resources/imagr_workflow.rb
+++ resources/imagr_workflow.rb
@@ -1,12 +1,21 @@
 resource_name :imagr_workflow
 
-property :name, String, name_property: true
+property :name, String, name_property: true, required: true
+property :description, String, default: ''
 property :restart_action, [NilClass, String], default: nil
 property :first_boot_reboot, [TrueClass, FalseClass], default: true
 property :hidden, [TrueClass, FalseClass], default: false
 property :bless_target, [TrueClass, FalseClass], default: true
 
-action :bind do
-  plist_structure = {}
-  plist_structure
+action :initialize do
+  plist = {}
+  plist['name'] = new_resource.name
+  plist['description'] = new_resource.description
+  plist['restart_action'] = new_resource.restart_action
+  plist['first_boot_reboot'] = new_resource.first_boot_reboot
+  plist['hidden'] = new_resource.hidden
+  plist['bless_target'] = new_resource.bless_target
+  plist.to_plist
+  f = File.out('imagr_config.plist', 'w')
+  f.write(plist)
 end
