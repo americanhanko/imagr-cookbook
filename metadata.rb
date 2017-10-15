dfb5073caae04fedda93c342363589577018cede
Eric Hanko <v-erhank@microsoft.com>
2017-10-14 19:19:09 -0700

    bump vers; solidify metadata

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,11 @@
 name 'imagr'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Eric Hanko'
+maintainer_email 'eric.hanko1@gmail.com'
+license 'MIT'
 description 'Installs/Configures imagr'
 long_description 'Installs/Configures imagr'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/imagr/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/imagr'
+version '0.2.0'
+chef_version '~> 13' if respond_to?(:chef_version)
+source_url 'https://github.com/americanhanko/imagr-cookbook'
+issues_url 'https://github.com/americanhanko/imagr-cookbook/issues'
+supports 'ubuntu'
--- recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: imagr
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
