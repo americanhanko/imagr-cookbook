c61bfd84e83030dc7c6b252424611c00e08c71f8
Eric Hanko <v-erhank@microsoft.com>
2017-10-14 19:15:55 -0700

    Update the basic files first
    I'm well aware this is why I need to be using a generator cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,17 +1,9 @@
-#
-# Cookbook:: imagr
-# Spec:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
 require 'spec_helper'
 
 describe 'imagr::default' do
   context 'When all attributes are default, on an Ubuntu 16.04' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
