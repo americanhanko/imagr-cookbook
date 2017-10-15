421ece965819a2535d05d1d9fd97d5ae4d37ef43
Eric Hanko <v-erhank@microsoft.com>
2017-10-14 19:59:47 -0700

    Add all root level configurations as attributes

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['imagr']['password'] = nil
+default['imagr']['autorun'] = nil
+default['imagr']['autorun_time'] = nil
+default['imagr']['default_workflow'] = nil
+default['imagr']['background_image'] = nil
+default['imagr']['wait_for_network'] = nil
+default['imagr']['restart_action'] = nil
