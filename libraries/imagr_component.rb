47772e4e4ecfbc06859445d9a36aa37a6725f5f2
Eric Hanko <v-erhank@microsoft.com>
2017-10-16 00:31:51 -0700

    Insert missing 'end' keyword

--- libraries/imagr_component.rb
+++ libraries/imagr_component.rb
@@ -1,20 +1,20 @@
 module ImagrCookbook
   class ImagrComponent < Chef::Resource
+    resource_name :imagr_component
 
-  resource_name :imagr_component
+    default_action :bind
 
-  default_action :bind
+    property :workflow_name, String, name_property: true
 
-  property :workflow_name, String, name_property: true
+    property :image, String
+    property :package, String
+    property :computer_name, String
+    property :localization, String
+    property :scripts, String
 
-  property :image, String
-  property :package, String
-  property :computer_name, String
-  property :localization, String
-  property :scripts, String
-
-  action :bind do
-    plist_structure = {}
-    plist_structure
+    action :bind do
+      plist_structure = {}
+      plist_structure
+    end
   end
 end
