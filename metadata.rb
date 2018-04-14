0ffd384bc96745a2808f76a53453a7a63f508bfe
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-04-13 17:54:57 -0700

    Update metadata to allow cookbook to support Chef 13 and greater

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Installs/Configures imagr'
 long_description 'Installs/Configures imagr'
 version '0.2.0'
-chef_version '~> 13' if respond_to?(:chef_version)
+chef_version '>= 13' if respond_to?(:chef_version)
 source_url 'https://github.com/americanhanko/imagr-cookbook'
 issues_url 'https://github.com/americanhanko/imagr-cookbook/issues'
 supports 'ubuntu'
