f5d415bbdc4cd5623923772dc3b6299e1148a97f
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-04-13 18:50:56 -0700

    Update default attribute for plist path

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,8 @@ default['imagr']['plist']['default_workflow'] = ''
 default['imagr']['plist']['background_image'] = ''
 default['imagr']['plist']['wait_for_network'] = ''
 default['imagr']['plist']['restart_action'] = ''
-
 default['imagr']['plist']['workflows'] = []
-default['imagr']['config']['path'] = "#{ENV['HOME']}/imagr_config.plist"
+default['imagr']['config']['plist'] = 'imagr_config.plist'
+
+default['imagr']['url'] = ''
+default['imagr']['app']['plist'] = '/Library/Preferences/com.grahamgilbert.Imagr.plist'
