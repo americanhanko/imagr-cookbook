096adfb583537cb47565373c2fe50d37bfd8844c
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-04-14 13:12:10 -0700

    Add integration tests for Imagr.app and launchd service

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -2,3 +2,13 @@ describe file('/Library/Preferences/com.grahamgilbert.Imagr.plist') do
   it { should exist }
   its('content') { should match %r{http(|s):\/\/\S+\/\S+.plist} }
 end
+
+describe directory('/Applications/Imagr.app') do
+  it { should exist }
+end
+
+describe launchd_service('imagr') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
