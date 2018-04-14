1e7bbb37eebd5138d9bd7a96096fdf7c5dbe3b42
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-04-13 19:57:36 -0700

    Mount the DMG as Imagr not Imagr.app

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,6 @@ template node['imagr']['app']['plist'] do
   source 'com.grahamgilbert.Imagr.erb'
 end
 
-dmg_package 'Imagr.app' do
+dmg_package 'Imagr' do
   source 'https://github.com/grahamgilbert/imagr/releases/download/1.4.1/Imagr-1.4.1.dmg'
 end
