4f8a52f133ac82e9c0c6bbe528cc822ca4993a1e
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-04-13 19:33:03 -0700

    Add dmg_package resource for the release of Imagr.app
    This is only available in Chef >= 14

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,7 @@
 template node['imagr']['app']['plist'] do
   source 'com.grahamgilbert.Imagr.erb'
 end
+
+dmg_package 'Imagr.app' do
+  source 'https://github.com/grahamgilbert/imagr/releases/download/1.4.1/Imagr-1.4.1.dmg'
+end
